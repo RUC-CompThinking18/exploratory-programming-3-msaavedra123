20310648e2c5564a0d75cbf1dc1331c872a99a43
msaavedra123 <43019009+msaavedra123@users.noreply.github.com>
2018-10-25 15:31:23 -0400

    Update matchingWords.py

--- matchingWords.py
+++ matchingWords.py
@@ -1,15 +1,16 @@
 import re
-source = open('Dracula.txt')			#Opens text file 'Dracula.txt' so words can be read and regex can be used
+source = open('Dracula.txt', 'r')			#Opens text file 'Dracula.txt' so words can be read and regex can be used
 onlineBook = source.read()
-#print matchingWords
+#print onlineBook
 
 def amess(onlineBook):
 	if type(onlineBook) != str:						#Type error makes sure argument is a string
 		raise TypeError("Not a string!")
 
 	match = re.findall(r"\w*at\b", onlineBook)			#Reg. expression that should find words ending with at in source file
-	aboveThree =  []											#Empty list to hold words ending in at that are larger than 3
+	aboveThree =  []											#Empty list to hold words ending in at
 	for word in match:
-		if len(word) >  3:						#If the words end in at and they contain four or more letters
-			aboveThree.append(word)				#then add them to the empty list aboveThree
+		if len(word) >  3:
+			aboveThree.append(word)
 	print(aboveThree)
+amess(onlineBook)
