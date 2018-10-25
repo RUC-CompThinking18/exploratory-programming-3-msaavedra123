de19cbb86d86ea536af060b0f2f1d838ac52a4c4
msaavedra123 <43019009+msaavedra123@users.noreply.github.com>
2018-10-25 14:45:26 -0400

    Update matchingWords.py

--- matchingWords.py
+++ matchingWords.py
@@ -8,8 +8,8 @@ def amess(onlineBook):
 		raise TypeError("Not a string!")
 
 	match = re.findall(r"\w*at\b", onlineBook)			#Reg. expression that should find words ending with at in source file
-	aboveThree =  []											#Empty list to hold words ending in at
+	aboveThree =  []											#Empty list to hold words ending in at that are larger than 3
 	for word in match:
-		if len(word) >  3:
-			aboveThree.append(word)
+		if len(word) >  3:						#If the words end in at and they contain four or more letters
+			aboveThree.append(word)				#then add them to the empty list aboveThree
 	print(aboveThree)
